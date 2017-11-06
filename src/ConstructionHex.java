7a2e0ff46e5abb4ab3a543fdc504dbf87e2360f5
Kun Wang <wangkun1991@gmail.com>
2017-11-07 02:44:50 +1100

    first step of immigrate

---
+++ src/ConstructionHex.java
@@ -0,0 +1,267 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * Created by Kun on 2017/11/07.
+ *
+ *  Construct HexGame from a string describing game state.
+ *  The initial state of the game, described as a string according to the assignment specification.
+ *  To get the right construction of hexgame, it should get the right nooks,crannies,pieces
+ */
+public class ConstructionHex {
+
+    private static String nooks="";
+    private static String crannies="";
+    private static String piece="";
+    private static String game="";
+
+    //This construction is just for making the only one piece of the game
+    public static String getGame(){
+        do {
+            nooks = "";
+            crannies = "";
+            game = "";
+            piece = "";
+            game += getRightCranny() + getRightNook() + getPiece();
+        }while(!HexGame.legitimateGame(game));
+        return game;
+
+    }
+
+    //This construction is for making the different piece of the game by the giving argument i
+    public static String getGame(int i){
+        do {
+            nooks = "";
+            crannies = "";
+            game = "";
+            piece = "";
+            game += getRightCranny() + getRightNook() + getPiece(i);
+        }while(!HexGame.legitimateGame(game));
+        return game;
+
+    }
+
+    //This method is for getting the right number,right position and right direction of the nooks
+    public static String getRightNook(){
+
+        //String nooks="";
+
+        String[] a={"008","020","021","038","039","040","062","063","064","065","092","093","094","095","096"};
+        String[] b={"098","067","099","042","068","100","023","043","069","101","010","024","044","070","102"};
+        String[] c={"012","026","046","072","104","027","047","073","105","048","074","106","075","107","108"};
+        String[] d={"014","030","029","052","051","050","080","079","078","077","114","113","112","111","110"};
+        String[] e={"120","085","056","033","016","119","084","055","032","118","083","054","117","082","116"};
+        String[] f={"126","125","090","124","089","060","123","088","059","036","122","087","058","035","018"};
+        String[] s={"A","B","C","D","E","F"};
+
+
+//        Node[] id = new Node[217];
+//        Node.create(id);
+
+        Random r=new Random();
+        boolean flaga=true;
+        boolean flagb=true;
+        boolean flagc=true;
+        boolean flagd=true;
+        boolean flage=true;
+        boolean flagf=true;
+
+
+
+        do{
+            String index1= a[r.nextInt(a.length)];
+            String index2= a[r.nextInt(a.length)];
+            String index3= a[r.nextInt(a.length)];
+            String s1= s[r.nextInt(s.length)];
+            String s2= s[r.nextInt(s.length)];
+            String s3= s[r.nextInt(s.length)];
+            if(adjacent(index1,index2)&&adjacent(index1,index3)&&adjacent(index2,index3)){
+                flaga=false;
+                nooks+=""+index1+s1+index2+s2+index3+s3;
+
+            }
+
+        }while(flaga);
+
+        do{
+            String index1= b[r.nextInt(a.length)];
+            String index2= b[r.nextInt(a.length)];
+            String index3= b[r.nextInt(a.length)];
+
+            String s1= s[r.nextInt(s.length)];
+            String s2= s[r.nextInt(s.length)];
+            String s3= s[r.nextInt(s.length)];
+
+            if(adjacent(index1,index2)&&adjacent(index1,index3)&&adjacent(index2,index3)){
+                flagb=false;
+                nooks+=""+index1+s1+index2+s2+index3+s3;
+
+            }
+
+        }while(flagb);
+
+        do{
+            String index1= c[r.nextInt(a.length)];
+            String index2= c[r.nextInt(a.length)];
+            String index3= c[r.nextInt(a.length)];
+
+            String s1= s[r.nextInt(s.length)];
+            String s2= s[r.nextInt(s.length)];
+            String s3= s[r.nextInt(s.length)];
+
+            if(adjacent(index1,index2)&&adjacent(index1,index3)&&adjacent(index2,index3)){
+                flagc=false;
+                nooks+=""+index1+s1+index2+s2+index3+s3;
+
+            }
+
+        }while(flagc);
+
+        do{
+            String index1= d[r.nextInt(a.length)];
+            String index2= d[r.nextInt(a.length)];
+            String index3= d[r.nextInt(a.length)];
+
+            String s1= s[r.nextInt(s.length)];
+            String s2= s[r.nextInt(s.length)];
+            String s3= s[r.nextInt(s.length)];
+
+            if(adjacent(index1,index2)&&adjacent(index1,index3)&&adjacent(index2,index3)){
+                flagd=false;
+                nooks+=""+index1+s1+index2+s2+index3+s3;
+
+            }
+
+        }while(flagd);
+
+        do{
+            String index1= e[r.nextInt(a.length)];
+            String index2= e[r.nextInt(a.length)];
+            String index3= e[r.nextInt(a.length)];
+
+            String s1= s[r.nextInt(s.length)];
+            String s2= s[r.nextInt(s.length)];
+            String s3= s[r.nextInt(s.length)];
+
+            if(adjacent(index1,index2)&&adjacent(index1,index3)&&adjacent(index2,index3)){
+                flage=false;
+                nooks+=""+index1+s1+index2+s2+index3+s3;
+
+            }
+
+        }while(flage);
+
+        do{
+            String index1= f[r.nextInt(a.length)];
+            String index2= f[r.nextInt(a.length)];
+            String index3= f[r.nextInt(a.length)];
+
+            String s1= s[r.nextInt(s.length)];
+            String s2= s[r.nextInt(s.length)];
+            String s3= s[r.nextInt(s.length)];
+
+            if(adjacent(index1,index2)&&adjacent(index1,index3)&&adjacent(index2,index3)){
+                flagf=false;
+                nooks+=""+index1+s1+index2+s2+index3+s3;
+
+            }
+
+        }while(flagf);
+
+
+        //System.out.println(nooks);
+
+        return nooks;
+
+    }
+    //This method is for distinguish whether the nooks are adjacent or not
+    public static boolean adjacent(String a,String b){
+
+        Node[] id = new Node[217];
+        Node.create(id);
+
+        int a0=Integer.parseInt(a);
+        int b0=Integer.parseInt(b);
+
+        if(a0==b0){
+            return false;
+        }
+
+        int x=id[a0].getRow()-id[b0].getRow();
+        int y=id[a0].getColumn()-id[b0].getColumn();
+        if ((x == 0 && y == 1) || (x == 0 && y == -1) || (x == 1 && y == 0)
+                || (x == -1 && y == 0) || (x == 1 && y == 1) || (x == -1 && y == -1)){
+            return false;
+        }
+
+        return true;
+    }
+
+
+    //This method is for getting the right number,right position of the crannies
+    public static String getRightCranny(){
+
+        //  String crannies="";
+        int[] a={169,170,171,172,173,174,175,176};
+        int[] b={177,178,179,180,181,182,183,184};
+        int[] c={185,186,187,188,189,190,191,192};
+        int[] d={193,194,195,196,197,198,199,200};
+        int[] e={201,202,203,204,205,206,207,208};
+        int[] f={209,210,211,212,213,214,215,216};
+
+        Random r=new Random();
+        crannies+=a[r.nextInt(a.length)];
+        crannies+=b[r.nextInt(a.length)];
+        crannies+=c[r.nextInt(a.length)];
+        crannies+=d[r.nextInt(a.length)];
+        crannies+=e[r.nextInt(a.length)];
+        crannies+=f[r.nextInt(a.length)];
+
+
+        return crannies;
+    }
+
+
+    //This method is for getting the right one piece
+    public static String getPiece(){
+        // String piece="";
+        // String nooks=getRightNook();
+        String[] nks=new String[18];
+        for (int i = 0; i < 18;i++) {
+            nks[i] = nooks.substring(4 * i, 4 * i + 3);
+        }
+        Random random=new Random();
+        piece=""+nks[random.nextInt(nks.length)];
+
+        return piece;
+
+    }
+
+    //This method is for getting the right number of pieces by giving the arguments k
+    public static String getPiece(int k){
+        // String piece="";
+
+        String[] nks=new String[18];
+        for (int i = 0; i < 18;i++) {
+            nks[i] = nooks.substring(4 * i, 4 * i + 3);
+        }
+
+        Set<String> set=new HashSet<>(k);
+        do{
+            Random r=new Random();
+            set.add(nks[r.nextInt(nks.length)]);
+
+        }while(set.size()!=k);
+
+        for(String p:set){
+            piece=piece+p;
+
+        }
+
+
+        return piece;
+    }
+
+
+}
---
+++ src/HexGame.java
@@ -0,0 +1,316 @@
+import java.util.ArrayList;
+
+/**
+ * Created by kun on 2017/11/07.
+ */
+public class HexGame {
+
+
+    /**
+     * Construct HexGame from a string describing game state.
+     * @param game The initial state of the game, described as a string according to the assignment specification.
+     */
+    private String game;
+
+    public HexGame(String game) {
+
+        if(legitimateGame(game))
+            this.game = game;
+        /* FIXME */
+    }
+
+    /**
+     * Construct HexGame with a random game state that complies with the assignment specification.
+     */
+    public HexGame() {
+        /* FIXME */
+        this.game = ConstructionHex.getGame();
+    }
+
+    /**
+     * Determine whether a set of crannies are legal according to the assignment specification.
+     * @param crannies A string describing the crannies, encoded according to the assignment specification.
+     * @return True if the crannies are correctly encoded and in legal positions, according to the assignment specification.
+     */
+    public static boolean legitimateCrannies(String crannies) {
+        /* FIXME */
+        if (crannies.length() != 18){
+            return false;
+        }
+        int[]cra = new int[6];
+        int[]mrk = new int[6];
+        for (int i = 0;i < 6;i++){
+            cra[i] = Integer.parseInt(crannies.substring(3*i,3*i + 3));
+            int m = cra[i] / 8 - 21;
+            if (cra[i] % 8 == 0){
+                m--;
+            }
+            if (m >= 0 && m <= 5){
+                mrk[m] = 1;
+            }
+        }
+        if (mrk[0]==1 && mrk[1]==1 && mrk[2]==1 && mrk[3]==1 && mrk[4]==1 && mrk[5]==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether a set of nooks are legal according to the assignment specification.
+     * @param nooks A string describing the nooks, encoded according to the assignment specification.
+     * @return True if the nooks are correctly encoded and in legal positions, according to the assignment specification.
+     */
+    public static boolean legitimateNooks(String nooks) {
+        /* FIXME */
+        if (nooks.length() != 72){
+            return false;
+        }
+        int[] nks = new int[18];
+        char[] psn = new char[18];
+        for (int i = 0; i < 18;i++){
+            nks[i] = Integer.parseInt(nooks.substring(4*i,4*i + 3));
+            if (!Nook.legal(nks[i])){
+                return false;
+            }
+            psn[i] = nooks.charAt(4*i + 3);
+           if(psn[i]!='A' && psn[i]!='B' && psn[i]!='C' && psn[i]!='D' && psn[i]!='E' && psn[i]!='F'){
+                return false;
+            }
+        }
+        int a = 0; int b = 0; int c = 0; int d = 0; int e = 0; int f = 0;
+        int[][]th = new int[6][18];
+        for(int i = 0;i < 18;i++){
+            if(Nook.legalplus(nks[i]) == 1){
+               th[0][a] = nks[i];
+               a++;
+            }
+            if(Nook.legalplus(nks[i]) == 2){
+                th[1][b] = nks[i];
+                b++;
+            }
+            if(Nook.legalplus(nks[i]) == 3){
+                th[2][c] = nks[i];
+                c++;
+            }
+            if(Nook.legalplus(nks[i]) == 4){
+                th[3][d] = nks[i];
+                d++;
+            }
+            if(Nook.legalplus(nks[i]) == 5){
+                th[4][e] = nks[i];
+                e++;
+            }
+            if(Nook.legalplus(nks[i]) == 6){
+                th[5][f] = nks[i];
+                f++;
+            }
+        }
+        if(a != 3 || b != 3 || c != 3 || d!= 3|| e!= 3 || f != 3){
+            return false;
+        }
+        Node[]id = new Node[217];
+        Node.create(id);
+        for (int i = 0;i < 6;i++){
+            for (int j = 0;j < 2;j++){
+                for (int k = j + 1;k < 3;k++){
+                    int x = id[th[i][j]].getRow() - id[th[i][k]].getRow();
+                    int y = id[th[i][j]].getColumn() - id[th[i][k]].getColumn();
+                    if ((x == 0 && y == 1) || (x == 0 && y == -1) || (x == 1 && y == 0)
+                            || (x == -1 && y == 0) || (x == 1 && y == 1) || (x == -1 && y == -1)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether a game state is legal according to the assignment specification.
+     * @param game A string describing the game state, encoded according to the assignment specification.
+     * @return True if the game state is correctly encoded and represents a legal game state, according to the assignment specification.
+     */
+    public static boolean legitimateGame(String game) {
+        /* FIXME */
+
+        //create databese
+        int[] nks = new int[18];
+        Node.createNks(game, nks);
+        //number of pieces
+        int num = (game.length() - 90) / 3;
+        int[] p = new int[num];
+        for (int i = 0;i < num; i++){
+            p[i] = Integer.parseInt(game.substring(90 + 3 * i,93 + 3 * i));
+            if (!Nook.nk(game, p[i])){
+                return false;
+            }
+        }
+        for (int i = 0;i < num;i++){
+            for (int j = i + 1;j < num;j++) {
+                if (p[i] == p[j]){                          //double pieces in same Node
+                    return false;
+                }
+            }
+        }
+
+        game = game.substring(0,90);
+
+        ArrayList<Integer> dictionary = new ArrayList<Integer>();
+        dictionary.add(169);
+
+        int key = 0;
+        int key1 = 0;
+        int key2 = 0;
+
+        for (int i = 0;i < dictionary.size();i++){
+            for (int j = 1;j < 7;j++){
+                int r = Node.move(game, dictionary.get(i), j);
+                int l = 0;
+                for (int k = 0;k < dictionary.size();k++){
+                    if (r == dictionary.get(k)){
+                        l = 1;
+                        break;
+                    }
+                }
+                if (l == 1){
+                    continue;
+                }
+                for (int k = 0;k < 18;k++){
+                    if (r == nks[k])
+                        key++;
+                }
+                if (r == 177 ){
+                    key1++;
+                }
+                if (r == 185){
+                    key2++;
+                }
+                dictionary.add(r);
+                if (key1 == 1 && key2 == 1 && key == 18){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether a given step is legal according to a given game state and the assignment specification.
+     * @param game A string describing the game state, encoded according to the assignment specification.
+     * @param from The point from which the step starts
+     * @param to The point to which step goes
+     * @return True if the move is legal according to the assignment specification.
+     */
+    public static boolean legitimateStep(String game, int from, int to) {
+        /* FIXME */
+
+        if (!Node.line(from,to) || from == to){
+            return false;
+        }
+
+        String pieces = game.substring(90);
+        int n = pieces.length()/3;
+        int flag = 0;
+        for (int i = 0;i < n;i++){
+            if (from == Integer.parseInt(pieces.substring(3 * i, 3 * i + 3))){
+                flag = 1;
+            }
+        }
+        if (flag == 0){
+            return false;
+        }
+        int drc = Node.direction(from, to);
+        int k = Node.move(game, from, drc);
+        return (k == to);
+    }
+
+    /**
+     * Return a minimal path from start to goal.
+     * @param game A string describing the game state, encoded according to the assignment specification.
+     * @param start The point from which the path must start
+     * @param goal The point at which the path must end
+     * @return An array of integers reflecting a minimal path from start to goal, each integer reflecting a Node on the board, starting with the start, and ending with the goal.
+     */
+//    public static int[] minimalPath(String game, int start, int goal) {
+//        /* FIXME */
+//
+//        //Because I changed Node.move, the string to set pieces will influnce the judgement,
+//        //so we cut the string "game"
+//        //this method is only for single pieces game now.
+//        game = game.substring(0,90);
+//
+//        ArrayList<ArrayList<Point>> Path = new ArrayList<ArrayList<Point>>();
+//        ArrayList<Point> zero = new ArrayList<Point>();
+//        Point Ancestry = new Point(start,null);
+//        zero.add(Ancestry);
+//        ArrayList<Point> allpoints = new ArrayList<Point>();
+//        allpoints.add(Ancestry);
+//        Path.add(zero);
+//
+//
+//
+//        //i 表示 List 中的第几个List
+//        //j 表示 当前起点List的角标
+//        //
+//        //Path.get(i) 表示当前起点List
+//        //
+//        //Path.get(i＋1) 储存当前终点
+//
+//
+//
+//        for (int i = 0;i < Path.size();i++){
+//
+//            //this is the new list to store new points,Path.get(i＋1)
+//            ArrayList<Point> sons = new ArrayList<Point>();
+//            Path.add(sons);
+//            //this is the jth start-point in list i
+//            for (int j = 0;j < Path.get(i).size();j++){
+//                int parentvalue = Path.get(i).get(j).getValue();
+//                for (int k = 1;k < 7;k++){
+//                    int childrenvalue = Node.move(game,parentvalue,k);
+//                    int flag = 0;
+//                    for (int l = 0;l < allpoints.size();l++){
+//                        if (allpoints.get(l).getValue() == childrenvalue){
+//                            flag = 1;
+//                        }
+//                    }
+//                    //if the childrenvalue is a new point, add new point to Path.get(i+1) and allpoints
+//                    if (flag == 0){
+//                        Point child = new Point(childrenvalue,Path.get(i).get(j));
+//                        sons.add(child);
+//                        allpoints.add(child);
+//                    }
+//                    if (childrenvalue == goal){
+//                        int length = Path.size();
+//                        int size = Path.get(i+1).size();
+//                        int[] recording = new int[length];
+//                        Point record = Path.get(i+1).get(size-1);
+//                        for (int l = length-1;l >= 0;l--){
+//                            recording[l] = record.getValue();
+//                            if (l != 0) {
+//                                Point.findparent(record);
+//                            }
+//                        }
+//                        return recording;
+//                    }
+//                }
+//            }
+//        }
+//
+//
+//        return null;
+//    }
+
+    /**
+     * Output the state of the game as a string, encoded according to the assignment specification
+     * @return A string that reflects the game state, encoded according to the assignment specification.
+     */
+    public String toString() {
+        /* FIXME */
+        return this.game;
+    }
+
+
+}
